db0db2f936e53dddef87d9923ceb9970ef9589da
Joao-Ex-Machina <joaobarreiroscoelhorodrigues@tecnico.ulisboa.pt>
2024-10-05 02:46:54 +0100

    Continued fine-tuning, very tired going to zzz

--- Project_1/Project.py
+++ Project_1/Project.py
@@ -8,22 +8,22 @@ FS = FuzzySystem()
 
 # Antecedents
 # HW Subsystem
-M1 = FuzzySet( points=[[0, 1],  [0.25, 0]], term="low" )
-M2 = FuzzySet( points=[[0.2, 0], [0.35, 1], [0.60,0]], term="average" )
-M3 = FuzzySet( points=[[0.5, 0], [0.70, 1.],[0.85,0]],term="high" )
-M4 = FuzzySet( points=[[0.8, 0], [1, 1]], term="critical" )
+M1 = FuzzySet( points=[[0, 1] , [0.3, 0]], term="low" )
+M2 = FuzzySet( points=[[0.3, 0], [0.5, 1], [0.65,0]], term="average" )
+M3 = FuzzySet( points=[[0.55, 0], [0.7, 1.],[0.85,0]],term="high" )
+M4 = FuzzySet( points=[[0.8, 0], [0.85, 1]], term="critical" )
 FS.add_linguistic_variable("Memory", LinguisticVariable([M1, M2, M3, M4], universe_of_discourse=[0, 1]))
 
-P1 = FuzzySet( points=[[0, 1],  [0.25, 0]], term="low" )
-P2 = FuzzySet( points=[[0.2, 0], [0.35, 1], [0.60,0]], term="average" )
-P3 = FuzzySet( points=[[0.5, 0], [0.70, 1.],[0.85,0]],term="high" )
-P4 = FuzzySet( points=[[0.8, 0], [1, 1]], term="critical" )
+P1 = FuzzySet( points=[[0, 1],  [0.3, 0]], term="low" )
+P2 = FuzzySet( points=[[0.25, 0], [0.5, 1], [0.65,0]], term="average" )
+P3 = FuzzySet( points=[[0.55, 0], [0.70, 1.],[0.85,0]],term="high" )
+P4 = FuzzySet( points=[[0.8, 0], [0.85, 1]], term="critical" )
 FS.add_linguistic_variable("Processor", LinguisticVariable([P1, P2, P3, P4], universe_of_discourse=[0, 1]))
 
-SP1 = FuzzySet(function=Triangular_MF(a=0,b=0,c=0.5), term="low")
-SP2 = FuzzySet(function=Triangular_MF(a=0.35,b=0.5,c=0.70), term="balanced")
-SP3 = FuzzySet(function=Triangular_MF(a=0.675,b=0.75,c=0.85), term="high")
-SP4 = FuzzySet(points=[[0.81, 0], [0.8, 1]], term="critical")
+SP1 = FuzzySet( points=[[0, 1], [0.3,1], [0.45, 0]] , term="low")
+SP2 = FuzzySet(function=Triangular_MF(a=0.3,b=0.45,c=0.6), term="balanced")
+SP3 = FuzzySet(function=Triangular_MF(a=0.55,b=0.65,c=0.8625), term="high")
+SP4 = FuzzySet(points=[[0.85, 0], [0.875, 1]], term="critical")
 FS.add_linguistic_variable("HW_Usage", LinguisticVariable([SP1, SP2, SP3, SP4], universe_of_discourse=[0, 1]))
 
 # HW Subsystem Rules
@@ -46,7 +46,7 @@ FS.add_rules([R_CRITICAL, R_HIGH, R_BAL1, R_BAL2, R_LOW1, R_LOW2])
 INP1 = FuzzySet( points=[[0, 1],  [0.25, 0]], term="low" )
 INP2 = FuzzySet( points=[[0.2, 0], [0.35, 1], [0.60,0]], term="average" )
 INP3 = FuzzySet( points=[[0.5, 0], [0.70, 1.],[0.85,0]], term="high" )
-INP4 = FuzzySet( points=[[0.75, 0], [1, 1]], term="very_high" )
+INP4 = FuzzySet( points=[[0.7, 0], [1, 1]], term="very_high" )
 FS.add_linguistic_variable("Input", LinguisticVariable([INP1, INP2, INP3, INP4], universe_of_discourse=[0, 1]))
 
 ONP1 = FuzzySet( points=[[0, 1],  [0.25, 0]], term="low" )
@@ -57,7 +57,7 @@ FS.add_linguistic_variable("Output", LinguisticVariable([ONP1, ONP2, ONP3, ONP4]
 
 
 N1 = FuzzySet(function=Triangular_MF(a=0,b=0,c=0.375), term="congested")
-N2 = FuzzySet(function=Triangular_MF(a=0.125,b=0.5,c=0.875), term="balanced")
+N2 = FuzzySet(function=Triangular_MF(a=0.25,b=0.5,c=0.75), term="balanced")
 N3 = FuzzySet(function=Triangular_MF(a=0.675,b=1,c=1), term="de_congested")
 FS.add_linguistic_variable("Network", LinguisticVariable([N1, N2, N3], universe_of_discourse=[0, 1]))
 
@@ -74,7 +74,7 @@ R_BAL3 = "IF (Input IS very_high) AND (Output IS very_high) THEN (Network IS bal
 
 R_CON1 = "IF (Output IS low) AND (NOT(Input IS low)) THEN (Network IS congested)"
 R_CON2 = "IF ((Input IS high) OR (Input IS very_high)) AND (Output IS average) THEN (Network IS congested)"
-R_CON3 = "IF (Input IS very_high) AND (Output IS high) THEN (Network IS congested)"
+R_CON3 = "IF (Input IS very_high) AND (NOT(Output IS very_high)) THEN (Network IS congested)"
 
 
 FS.add_rules([R_DECON1, R_DECON2, R_DECON3, R_BAL1, R_BAL2, R_BAL3, R_CON1, R_CON2, R_CON3])
@@ -82,28 +82,28 @@ FS.add_rules([R_DECON1, R_DECON2, R_DECON3, R_BAL1, R_BAL2, R_BAL3, R_CON1, R_CO
 #Subsystem3
 
 B1 = FuzzySet( points=[[0, 1],  [0.25, 0]], term="low" )
-B2 = FuzzySet( points=[[0.2, 0], [0.35, 1], [0.60,0]], term="average" )
-B3 = FuzzySet( points=[[0.5, 0], [0.70, 1.],[0.85,0]], term="high" )
-B4 = FuzzySet( points=[[0.75, 0], [1, 1]], term="very_high" )
+B2 = FuzzySet( points=[[0.2, 0], [0.4, 1], [0.60,0]], term="average" )
+B3 = FuzzySet( points=[[0.5, 0], [0.6, 1.],[0.7,0]], term="high" )
+B4 = FuzzySet( points=[[0.65, 0], [0.8, 1]], term="very_high" )
 FS.add_linguistic_variable("Bandwidth", LinguisticVariable([B1, B2, B3, B4], universe_of_discourse=[0, 1]))
 
-NS1 = FuzzySet(function=Triangular_MF(a=0,b=0,c=0.35), term="high")
-NS2 = FuzzySet(function=Triangular_MF(a=0.30,b=0.5,c=0.75), term="balanced")
-NS3 = FuzzySet(function=Triangular_MF(a=0.70,b=1,c=1), term="low")
-FS.add_linguistic_variable("TrueNetCongestion", LinguisticVariable([NS1, NS2, NS3], universe_of_discourse=[0, 1]))
+TNC1 = FuzzySet(function=Triangular_MF(a=0,b=0,c=0.35), term="fullycongested")
+TNC2 = FuzzySet(function=Triangular_MF(a=0.30,b=0.5,c=0.75), term="balanced")
+TNC3 = FuzzySet(function=Triangular_MF(a=0.70,b=1,c=1), term="decongestable")
+FS.add_linguistic_variable("TrueNetCongestion", LinguisticVariable([TNC1, TNC2, TNC3], universe_of_discourse=[0, 1]))
 
 
 
 #Subsystem Rule3
 
-R_NCON1 = "IF (Network IS congested) AND (NOT(Bandwidth IS very_high)) THEN (TrueNetCongestion IS high)"
-R_NCON2 = "IF (Network IS congested) AND (Bandwidth IS very_high) THEN (TrueNetCongestion IS balanced)" 
+R_NCON1 = "IF (Network IS congested) AND (NOT(Bandwidth IS very_high)) THEN (TrueNetCongestion IS fullycongested)"
+R_NCON2 = "IF (Network IS congested) AND (Bandwidth IS very_high) THEN (TrueNetCongestion IS decongestable)" 
 
 R_NBAL1 = "IF (Network IS balanced) AND (NOT((Bandwidth IS low) OR (Bandwidth IS very_high))) THEN (TrueNetCongestion IS balanced)"
-R_NBAL2 = "IF (Network IS balanced) AND (Bandwidth IS very_high) THEN (TrueNetCongestion IS low)"
-R_NBAL3 = "IF (Network IS balanced) AND (Bandwidth IS low) THEN (TrueNetCongestion IS high)"
+R_NBAL2 = "IF (Network IS balanced) AND (Bandwidth IS very_high) THEN (TrueNetCongestion IS decongestable)"
+R_NBAL3 = "IF (Network IS balanced) AND (Bandwidth IS low) THEN (TrueNetCongestion IS fullycongested)"
 
-R_NDECON1 = "IF (Network IS de_congested) AND (Bandwidth IS low) THEN (TrueNetCongestion IS balanced)"
+R_NDECON1 = "IF (Network IS de_congested) AND (Bandwidth IS low) THEN (TrueNetCongestion IS decongestable)"
 R_NDECON2 = "IF (Network IS de_congested) AND (NOT(Bandwidth IS low)) THEN (TrueNetCongestion IS balanced)" 
 
 
@@ -113,24 +113,24 @@ FS.add_rules([R_NCON1, R_NCON2, R_NBAL1, R_NBAL2, R_NBAL3, R_NDECON1, R_NDECON2]
 # Super-Fuzzy System 
 
 #Big Power variable
-L1 = FuzzySet( points=[[0, 1],  [0.25, 0]], term="low" )
-L2 = FuzzySet( points=[[0.2, 0], [0.35, 1], [0.65,0]], term="average" )
-L3 = FuzzySet( points=[[0.55, 0], [0.75, 1]], term="high" )
+L1 = FuzzySet( points=[[0, 1],  [0.3, 0]], term="low" )
+L2 = FuzzySet( points=[[0.25, 0], [0.5, 1], [0.7,0]], term="average" )
+L3 = FuzzySet( points=[[0.675, 0], [0.8, 1]], term="high" )
 FS.add_linguistic_variable("Latency", LinguisticVariable([L1, L2, L3], universe_of_discourse=[0, 1]))
 
 
 # Consequents
 CLP1 = FuzzySet(function=Triangular_MF(a=-1,b=-1,c=-0.3), term="decrease")
-CLP2 = FuzzySet(function=Triangular_MF(a=-0.35,b=0,c=0.35), term="keep")
-CLP3 = FuzzySet(function=Triangular_MF(a=0.3,b=1,c=1), term="increase")
+CLP2 = FuzzySet(function=Triangular_MF(a=-0.35,b=0,c=0.45), term="keep")
+CLP3 = FuzzySet(function=Triangular_MF(a=0.4,b=1,c=1), term="increase")
 FS.add_linguistic_variable("CLP", LinguisticVariable([CLP1, CLP2, CLP3], universe_of_discourse=[-1, 1]))
 
 R_HWLow= "IF (HW_Usage IS low) THEN (CLP IS increase)"
 R_HWCritical= "IF (HW_Usage IS critical) THEN (CLP IS decrease)"
 R_HighLatency1 = "IF (Latency IS high) AND (NOT(HW_Usage IS critical)) THEN (CLP IS increase)"
-R_NetCongested = "IF (TrueNetCongestion IS high) AND (NOT((HW_Usage IS low) OR (Latency IS high))) THEN (CLP IS decrease)"
-R_NetDeCongested = "IF (TrueNetCongestion IS low) AND (NOT(HW_Usage IS critical)) THEN (CLP IS increase)"
-R_HWBAL1 = "IF (HW_Usage IS balanced) AND (NOT(TrueNetCongestion IS high)) THEN (CLP IS increase)"
+R_NetDeCongested = "IF (TrueNetCongestion IS decongestable) AND (NOT((HW_Usage IS low) OR (Latency IS high))) THEN (CLP IS decrease)"
+R_NetCongested = "IF (TrueNetCongestion IS fullycongested) AND (NOT(HW_Usage IS critical)) THEN (CLP IS increase)"
+R_HWBAL1 = "IF (HW_Usage IS balanced) AND (NOT(TrueNetCongestion IS decongestable)) THEN (CLP IS increase)"
 R_HWBAL2 = "IF (HW_Usage IS balanced) AND ((TrueNetCongestion IS balanced) AND (Latency IS low)) THEN (CLP IS keep)"
 R_HWHi1 = "IF (HW_Usage IS high) AND ((TrueNetCongestion IS balanced) AND (Latency IS low)) THEN (CLP IS decrease)"
 R_HWHi2 = "IF (HW_Usage IS high) AND ((TrueNetCongestion IS balanced) AND (Latency IS average)) THEN (CLP IS keep)"
