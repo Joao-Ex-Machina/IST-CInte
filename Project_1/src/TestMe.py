983631219d271b0ca637da24023b5ffb7a7f7b4e
David Xia <daviddanielxia@gmail.com>
2024-10-07 14:41:09 +0100

    Hyperparemeters tuning

--- Project_1/src/TestMe.py
+++ Project_1/src/TestMe.py
@@ -1,4 +1,5 @@
 import subprocess
+import joblib
 
 DEFAULT_debug="CINTE24-25_Proj1_SampleData.csv"
 DEFAULT="Proj1_TestS.csv"
@@ -6,3 +7,6 @@ DEFAULT="Proj1_TestS.csv"
 result = subprocess.run(["python", "Fuzzy_System.py", DEFAULT_debug], capture_output=True, text=True)
 
 print(result.stdout)
+
+clf = joblib.load('model_filename.pkl')
+
