b4e69dc81d9c9b9ea47f7d4b12d1faf1f8fc4afa
David Xia <daviddanielxia@gmail.com>
2024-10-07 21:37:51 +0100

    NN Final Update

--- Project_1/src/Neural_Network.py
+++ Project_1/src/Neural_Network.py
@@ -63,7 +63,7 @@ class_counts = Counter(Y_class)
 print(f"Class counts: {class_counts}")
 
 
-Validation = True
+Validation = False
 
 if(Validation == True):
     # Data Split Train 70% Test 15% Validation 15%
@@ -80,23 +80,23 @@ print(f"Class counts (Train): {class_counts_train}")
 #Training Neural Network with Cross Validation
 if(Validation == False):
     clf = MLPRegressor(hidden_layer_sizes=(8, 5), activation='tanh',solver='lbfgs',max_iter= 10000,random_state=0)
-    # kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
     # Perform cross-validation with scoring as MSE
-    # mse_scorer = make_scorer(mean_squared_error, greater_is_better=False)  # Negative because lower MSE is better
-    # mse_scores = cross_val_score(clf, X_train, Y_train, cv=kf, scoring=mse_scorer)
+    mse_scorer = make_scorer(mean_squared_error, greater_is_better=False)  # Negative because lower MSE is better
+    mse_scores = cross_val_score(clf, X_train, Y_train, cv=kf, scoring=mse_scorer)
 
-    # # Perform cross-validation with scoring as R² (default for regressors)
-    # r2_scores = cross_val_score(clf, X_train, Y_train, cv=kf) 
-    # print("Cross-Validated R² Scores: ", r2_scores)
-    # print(f"Mean R²: {np.mean(r2_scores)}")
+    # Perform cross-validation with scoring as R² (default for regressors)
+    r2_scores = cross_val_score(clf, X_train, Y_train, cv=kf) 
+    print("Cross-Validated R² Scores: ", r2_scores)
+    print(f"Mean R²: {np.mean(r2_scores)}")
 
-    # print("Cross-Validated MSE Scores (Negative): ", mse_scores)
-    # print(f"Mean MSE: {np.mean(np.abs(mse_scores))}")
+    print("Cross-Validated MSE Scores (Negative): ", mse_scores)
+    print(f"Mean MSE: {np.mean(np.abs(mse_scores))}")
     clf.fit(X_train, Y_train)
-    print ("Training Accuracy: ",clf.score(X_train,Y_train)) # R^2  
-    print("MSE: ", mean_squared_error(Y_train, clf.predict(X_train)))
+    # print ("Training Accuracy: ",clf.score(X_train,Y_train)) # R^2  
+    # print("MSE: ", mean_squared_error(Y_train, clf.predict(X_train)))
 
-number = 9
+number = 10
 #Grid Search for Hyperparameter Tuning
 if(Validation == True):
     param_grid = {
@@ -104,9 +104,6 @@ if(Validation == True):
         'activation': ['identity', 'logistic', 'tanh', 'relu'],
         'solver': ['lbfgs', 'sgd', 'adam'],
     }
-    df = pd.read_csv('Project_1//src//CINTE24-25_Proj1_SampleData.csv')
-    Y_test2 = df["CLPVariation"].values
-    X_test2 = df.iloc[:, 0:6].values
 
     Best_Accuracy = 0  
     Best_MSE = 1000 
@@ -114,10 +111,9 @@ if(Validation == True):
         for j in range(param_grid['activation'].__len__()):
             for k in range(param_grid['solver'].__len__()):
                 clf = MLPRegressor(hidden_layer_sizes=param_grid['hidden_layer_sizes'][i], activation=param_grid['activation'][j],solver=param_grid['solver'][k],max_iter= 1000,early_stopping=True,random_state=0).fit(X_train, Y_train)
-                # accuracy = clf.score(X_val, Y_val)
-                # mse = mean_squared_error(Y_val,clf.predict(X_val))
-                accuracy = clf.score(X_test2, Y_test2)
-                mse = mean_squared_error(Y_test2,clf.predict(X_test2))
+                accuracy = clf.score(X_val, Y_val)
+                mse = mean_squared_error(Y_val,clf.predict(X_val))
+
                 print(f"Accuracy: {accuracy}, MSE: {mse}, Hidden Layer Sizes: {param_grid['hidden_layer_sizes'][i]}, Activation: {param_grid['activation'][j]}, Solver: {param_grid['solver'][k]}")
                 if(Best_MSE > mse):
                     Best_MSE = mse
@@ -125,7 +121,7 @@ if(Validation == True):
                     best_param = [param_grid['hidden_layer_sizes'][i],param_grid['activation'][j],param_grid['solver'][k]]
                     best_clf = clf
 
-    print("Best Accuracy: " +  str(best_clf.score(X_test2,Y_test2)) + " MSE: " + str(mean_squared_error(Y_test2,best_clf.predict(X_test2))) + " Best Parameters: " + str(best_param))
+    print("Best Accuracy: " +  str(best_clf.score(X_val,Y_val)) + " MSE: " + str(mean_squared_error(Y_val,best_clf.predict(X_val))) + " Best Parameters: " + str(best_param))
     clf = best_clf
     
 
@@ -156,9 +152,3 @@ for i in range(len(Y_pred2)):
     print(f"Actual: {Y_test2[i]}, Actual Class: {Y_test2_class[i]}, Prediction: {Y_pred2[i]} Predicted class: {Y_pred2_class[i]}")
 
 print("Test MSE: ", mean_squared_error(Y_test2, Y_pred2))
-
-# Best Parameters: {'activation': 'relu', 'hidden_layer_sizes': (10, 12), 'solver': 'lbfgs'}
-# Best Accuracy: 0.9437004756337296 MSE: 0.026162163774908404 Best Parameters: [(11, 1), 'logistic', 'lbfgs']
-# Best Accuracy: 0.964070272935983 MSE: 0.01669640044774043 Best Parameters: [(5, 5), 'tanh', 'lbfgs']
-# Best Accuracy: 0.9552937531012275 MSE: 0.020774814108871952 Best Parameters: [(7, 5), 'tanh', 'lbfgs']
-# Best Accuracy: 0.9645428522137957 MSE: 0.01647679474765803 Best Parameters: [(8, 5), 'tanh', 'lbfgs']
