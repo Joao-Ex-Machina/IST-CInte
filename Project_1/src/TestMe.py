c5531a5176b493ad4270b4228f3bbd45aa244edd
David Xia <daviddanielxia@gmail.com>
2024-10-07 20:17:51 +0100

    NN result added to TestMe

--- Project_1/src/TestMe.py
+++ Project_1/src/TestMe.py
@@ -1,5 +1,6 @@
 import subprocess
 import joblib
+import pandas as pd
 
 DEFAULT_debug="CINTE24-25_Proj1_SampleData.csv"
 DEFAULT="Proj1_TestS.csv"
@@ -8,5 +9,11 @@ result = subprocess.run(["python", "Fuzzy_System.py", DEFAULT_debug], capture_ou
 
 print(result.stdout)
 
+data = pd.read_csv(DEFAULT)
 clf = joblib.load('model_filename.pkl')
+
+X = data.iloc[:, 0:6].values
+
+Y_pred = clf.predict(X)
+Y_pred = pd.DataFrame(Y_pred, columns=["NN_CLPVariation"])
 
